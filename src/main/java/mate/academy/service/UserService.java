24885669b9cf0fb3864247bcf7e9562f778f4fd7
Danyil <danil.shkodiuk@gmail.com>
2026-02-07 14:20:25 +0100

    Revert "ading new login logic to AuthenticationService.java and UserService.java"
    This reverts commit 0dad22311045f99b2a46ecaf9e0d18bb8419ae50.

--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -15,11 +15,6 @@ public class UserService {
      * Return <code>null</code> if there is no suitable user
      */
     public User findByEmail(String email) {
-        for (User user : users) {
-            if (email.equals(user.getEmail())) {
-                return user;
-            }
-        }
         return null;
     }
 }
